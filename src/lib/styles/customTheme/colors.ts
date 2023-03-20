4a0f91df5c7cb0a5e68b1d485f5f5f372516ab0b
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-03-20 18:57:58 +0700

    feat: update gray color theme

--- src/lib/styles/customTheme/colors.ts
+++ src/lib/styles/customTheme/colors.ts
@@ -4,16 +4,17 @@ import type { DeepPartial, Theme } from "@chakra-ui/react";
 const extendedColors: DeepPartial<
   Record<string, Theme["colors"]["blackAlpha"]>
 > = {
-  brand: {
-    100: "",
-    200: "",
-    300: "",
-    400: "",
-    500: "",
-    600: "",
-    700: "",
-    800: "",
-    900: "",
+  gray: {
+    50: "#e8e9e9",
+    100: "#d1d2d2",
+    200: "#a3a5a5",
+    300: "#747978",
+    400: "#464c4b",
+    500: "#181f1e",
+    600: "#131918",
+    700: "#0e1312",
+    800: "#0a0c0c",
+    900: "#050606",
   },
 };
 
